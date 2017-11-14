7c1c2c364b137c0eff89e143f5d0403653685e25
hpai1990 <hpai1990@gmail.com>
2017-11-14 10:07:23 +0530

    Scripts updated

--- src/test/java/com/selenium/test/DemoScript.java
+++ src/test/java/com/selenium/test/DemoScript.java
@@ -3,7 +3,7 @@ package com.selenium.test;
 public class DemoScript {
 	
 	public static void main(String[] args) {
-		System.out.println("Default Pipeline");
+		System.out.println("Default Pipeline Updated");
 	}
 
 }
